1e3ca51cbce67b7c597b5cebfa9436764ce6317f
A Rohl <april.rohl2015@gmail.com>
2020-02-24 18:30:05 -0700

    added spacing between : and #'s

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,7 +30,7 @@ function startGame() {
     $("#button-4").attr("value", randnum1);
     console.log(randnum1);
     random = Math.floor(Math.random() * 100) + 20;
-    $("#random").text("Match Number:" + random);
+    $("#random").text("Match Number: " + random);
     console.log(randnum1, random);
 }
 
@@ -41,18 +41,18 @@ $("button").on("click", function (event) {
     var gemValue = $(this).attr("value");
     score += parseInt(gemValue);
     console.log(score);
-    $("#score").text("Your Total Score:"  + score);
+    $("#score").text("Your Total Score: "  + score);
     if (score === random) {
         wins++;
         alert("You Won!");
-        $("#wins").text("Wins:"  + wins);
-        $("#score").text("Your Total Score:"  + score);
+        $("#wins").text("Wins: "  + wins);
+        $("#score").text("Your Total Score: "  + score);
         reset();
     }
     else if (score > random) {
         losses++;
         alert("You Lost :(")
-        $("#losses").text("Losses:" + losses);
+        $("#losses").text("Losses: " + losses);
         reset();
     }
 })
@@ -61,7 +61,7 @@ $("button").on("click", function (event) {
 function reset() {
     alert("Let's Collect More Crystals!");
     score = 0;
-    $("#score").text("Your Total Score:" + score);
+    $("#score").text("Your Total Score: " + score);
     startGame();
 }
 
